8ce03df6a34867c8329d19533008f1bba142659a
mijinB <bmj13465@gmail.com>
2023-09-06 17:12:42 +0900

    같은 Droppable(Board)에서만 드래그,드롭 적용되도록 구현

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,6 @@ import Board from "./components/Board";
 
 const Wrapper = styled.div`
     display: flex;
-    justify-content: center;
-    align-items: center;
     width: 100%;
     height: 100vh;
     max-width: 680px;
@@ -17,6 +15,8 @@ const Wrapper = styled.div`
 const Boards = styled.div`
     display: grid;
     grid-template-columns: repeat(3, 1fr);
+    justify-content: center;
+    align-items: center;
     gap: 10px;
     width: 100%;
     min-height: 200px;
@@ -27,20 +27,22 @@ function App() {
 
     /**@function onDragEnd
      * 1. event로 draggableId와 source, destination을 인자로 받고
-     * 2. destination의 유무 확인 후 없으면 아무것도 return하지 않고 함수 종료(제자리에 드롭했을 경우를 위함)
-     * 3. source.index(드래그한 item의 위치)에 있는 item 삭제
-     * 4. destination.index(드롭한 item의 위치)에 draggableId(드래그한 item, 삭제했던 item) 삽입
-     * 5. setToDos 사용해서 새로 생성한 List를 반환하여 toDos List 수정
+     * 2. source(드래그), destination(드롭)이 서로 draggableId(해당 위치의 boardId)가 일치하면 아래 기능 수행, 일치하지 않는다면 아무것도 하지않고 함수 종료
+     * 3. toDos의 3개 List 중에서 변화가 일어난 draggableId의 List만 복사해놓고
+     * 4. source.index(드래그한 item의 위치)에 있는 item 삭제
+     * 5. destination.index(드롭한 item의 위치)에 draggableId(드래그한 item, 삭제했던 item) 삽입
+     * 6. toDos의 기존 3개 List에서 변화가 일어난 List만 수정하여 toDos Object 반환(setToDos)
      */
     const onDragEnd = ({ draggableId, source, destination }: DropResult) => {
-        if (!destination) return;
-        /* setToDos((prevToDos) => {
-            const copyToDos = [...prevToDos];
-            copyToDos.splice(source.index, 1);
-            copyToDos.splice(destination?.index, 0, draggableId);
+        if (destination?.droppableId === source.droppableId) {
+            setToDos((prevToDos) => {
+                const boardCopy = [...prevToDos[source.droppableId]];
+                boardCopy.splice(source.index, 1);
+                boardCopy.splice(destination?.index, 0, draggableId);
 
-            return copyToDos;
-        }); */
+                return { ...prevToDos, [source.droppableId]: boardCopy };
+            });
+        }
     };
 
     return (
